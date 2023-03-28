5e9e7cdc20fc6d370f1ae3cc776c48598d78ff29
dylanperera1 <dperera@udel.edu>
2023-03-28 00:55:54 -0400

    implemented animation

--- MarioClone/Assets/Scripts/PlatformerPlayer.cs
+++ MarioClone/Assets/Scripts/PlatformerPlayer.cs
@@ -9,10 +9,12 @@ public class PlatformerPlayer : MonoBehaviour
     public float speed = 3.5f;
 
     private Rigidbody2D body;
+    private Animator anim;
 
     void Start()
     {
         body = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     void Update()
@@ -20,6 +22,11 @@ public class PlatformerPlayer : MonoBehaviour
         float deltaX = Input.GetAxis("Horizontal") * speed;
         Vector2 movement = new Vector2(deltaX, body.velocity.y);
         body.velocity = movement;
+
+        anim.SetFloat("speed", Mathf.Abs(deltaX));
+        if (!Mathf.Approximately(deltaX, 0)) {
+            transform.localScale = new Vector3(Mathf.Sign(deltaX), 1, 1);
+        }
     }
 
   
